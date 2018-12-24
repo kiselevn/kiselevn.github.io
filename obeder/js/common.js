92a6b452d207b25b11500dfed0539803a6c258c2
kiselevn <nikihka@icloud.com>
2018-12-24 10:59:40 +0400

    Add animation to display none. Corrections in the layout.

--- obeder/js/common.js
+++ obeder/js/common.js
@@ -1,17 +1,17 @@
-var rad = document.swithesForm.switch;
+// скрытие формы переключателем
+var rad = document.switchesForm.switch;
 var prev = null;
-var elems = document.getElementsByClassName("content");
+var content = document.getElementsByClassName("content");
+
 for(var i = 0; i < rad.length; i++) {
   rad[i].onclick = function() {
-    (prev)? console.log(prev.value):null;
     if(this !== prev) {
       prev = this;
     }
-    console.log(this.value);
     if (this.value == 'off') {
-      for(var i=0; i<elems.length; i++)elems[i].style.display = 'none';
+      for(var i=0; i<content.length; i++)content[i].style.display = 'none';
     } else {
-      for(var i=0; i<elems.length; i++)elems[i].style.display = 'grid';
+      for(var i=0; i<content.length; i++)content[i].style.display = 'grid';
     }
   };
 };
