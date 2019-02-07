dbbdef58a6c8bd62df0ad3ba2457c8d1eea6b555
kiselevn <nikihka@icloud.com>
2019-02-07 16:12:28 +0400

    correction of errors in semantics

---
+++ TwitterBox/js/main.js
@@ -0,0 +1,15 @@
+const MAX_LENGTH = 10;
+const input = document.querySelector('#tweet-message');
+const counter = document.querySelector('#counter');
+
+const setCounterLength = (len) => {
+  counter.textContent = `${len} из ${MAX_LENGTH}`; 
+}
+
+const hendleInputChange = (e) => {
+  
+}
+
+input.addEventListener('input', hendleInputChange);
+
+setCounterLength(0);
