8dc686404b620dd1fae8c191a7b0160b04541659
kiselevn <nikihka@icloud.com>
2019-02-08 17:36:49 +0400

    Added length message control and added adding image

--- TwitterBox/js/main.js
+++ TwitterBox/js/main.js
@@ -1,13 +1,64 @@
-// const MAX_LENGTH = 10;
-// const input = document.querySelector('#tweet-message');
-// const counter = document.querySelector('#counter');
+const MAX_LENGTH = 3;
+const IMAGE_LENGTH = 2;
+const input = document.querySelector('#tweet-message');
+const counter = document.querySelector('#counter');
+const button = document.querySelector('#button-send');
+const imageInput = document.querySelector('#loading-images');
+const imagePreview = document.querySelector('#image-preview');
+const imageList = document.querySelector('#image-list');
 
-// const setCounterLength = len => {
-//   counter.textContent = `${len} из ${MAX_LENGTH}`;
-// };
+const stat = {
+  messageCounter: 0,
+  imageCounter: 0,
+  image: []
+};
 
-// const hendleInputChange = e => {};
+const setCounterLength = len => {
+  counter.textContent = `${len} из ${MAX_LENGTH}`;
+};
 
-// input.addEventListener('input', hendleInputChange);
+const lengthControl = () => {
+  const totalLength = stat.messageCounter + stat.imageCounter * IMAGE_LENGTH;
 
-// setCounterLength(0);
+  if (totalLength > MAX_LENGTH) {
+    counter.classList.add('max-length');
+    button.disabled = true;
+  } else {
+    counter.classList.remove('max-length');
+    button.disabled = false;
+  }
+
+  setCounterLength(totalLength);
+};
+
+const handleInputChange = () => {
+  stat.messageCounter = input.value.length;
+  lengthControl();
+};
+
+const handleImageChange = event => {
+  const reader = new window.FileReader();
+  const file = event.target.files[0];
+
+  reader.onload = function exp(e) {
+    stat.image += `<li><img src="${e.target.result}"/></li>`;
+    imageList.innerHTML = stat.image;
+    // console.log(stat.image);
+    stat.imageCounter += 1;
+    lengthControl();
+  };
+
+  reader.readAsDataURL(file);
+};
+
+const deleteImage = event => {
+  console.log(event);
+  debugger;
+  stat.imageCounter -= 1;
+};
+
+input.addEventListener('input', handleInputChange);
+imageInput.addEventListener('input', handleImageChange);
+imagePreview.addEventListener('click', deleteImage);
+
+lengthControl();
